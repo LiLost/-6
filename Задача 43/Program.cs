01d6a3e52042a38f65ec051f0fe7477473dd830f
Liliya <liliya.demchik.13@mail.ru>
2022-10-30 16:24:01 +0500

    все

---
+++ Задача 43/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователе;.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> ( -0,5; -0,5)
+
+void TwoStrong(double b1, double k1, double b2, double k2)
+{
+    if (k1 != k2)
+    {
+        double numX = k2 - k1;
+        double num = b1 - b2;
+        double X = num / numX;
+        double Y = k2 * (X) + b2;
+        Console.WriteLine($"Точка пересечения этих двух прямых({X}; {Y})");
+    }
+    else
+    Console.WriteLine($"Не пересекаются");
+}
+
+Console.Clear();
+Console.WriteLine("Введите b1 ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1 ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2 ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2 ");
+double k2 = double.Parse(Console.ReadLine()!);
+TwoStrong(b1, k1, b2, k2);
---
+++ Задача со звездочкой/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача со звездочкой: Напишите программу, которая реализует обход введенного двумерного массива,
+// начиная с крайнего нижнего левого элемента против часовой стрелки.
+// 1 2 3
+// 4 5 6 -> 7 8 9 6 3 2 1 4 5
+// 7 8 9
+
+int[,] matrix = new int[3, 4];
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10);
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+PrintArray(matrix);
